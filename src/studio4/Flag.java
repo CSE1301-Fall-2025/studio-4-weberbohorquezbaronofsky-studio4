2d20396ce4c6588f6e7f057971e959727efccaad
Will Weber <weber.w@wustl.edu>
2025-10-01 14:09:00 -0500

    studio4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		StdDraw.setPenColor(145,201,104);
+		StdDraw.filledRectangle(11, 8, 11, 8);
+		StdDraw.setPenColor(210,64,181);
+		StdDraw.filledRectangle(11, 8, 11, 6);
+		StdDraw.filledRectangle(11, 8, 9, 8);
+		StdDraw.setPenColor(14,150,92);
+		StdDraw.filledCircle(11, 8, 3);
+
 		
 	}
 }
